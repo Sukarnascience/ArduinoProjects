629d7a2191c9094b00a5380d02eab4003c23c922
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2022-04-10 19:21:59 +0530

    Update mainServer.py

--- PU_P1/mainServer.py
+++ PU_P1/mainServer.py
@@ -20,11 +20,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from random import random
 from socket import timeout
 from warnings import catch_warnings
 import serial
 import time
 import cv2
+import random
+import time
+
 
 # [MAIN]
 # if mode is 1 it means it will take data from goggles 
@@ -40,20 +44,40 @@ EyeHaarcascade = "/home/sukarna/Documents/PU_P1/haarcascade_eye.xml"
 # Config the arduino board
 Port = "/dev/ttyUSB0"
 BoardRate = 9600
+moved = 0
+mode = 0
 
 try:
-    board  = serial.Serial(port=Port, baudrate=BoardRate, timeout=1)
+    board  = serial.Serial(
+        port=Port, 
+        baudrate=BoardRate, 
+        #parity=serial.PARITY_NONE,
+        #stopbits=serial.STOPBITS_ONE,
+        #bytesize=serial.EIGHTBITS,
+        timeout=1 )
+    board.flushInput()
+    print("Connected to arduino")
 except:
     print("Fail to connect to arduino")
 
 def sendData(data):
+    time.sleep(0.1)
     try:
-        if(data):
-            board.write(b'10')
+        print(data)
+        if(data==1):
+            #garbageValue = random.randint(12,99)
+            #board.write(b'1')
+            board.write(b'RRRR')
+        elif(data==11):
+            #board.write(b'11')
+            board.write(b'S')
         else:
-            board.write(b'11')
+            #board.write(b'10')
+            board.write(b'E')
+            
     except:
         print("Fail to send")
+    #board.close()
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier(FaceHaarcascade)
@@ -66,24 +90,33 @@ while True:
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray,1.1, 3)
+    faces = face_cascade.detectMultiScale(gray,1.1, 5)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Detect the faces
-        faces = eye_cascade.detectMultiScale(gray,1.2, 4)
+        eyes = eye_cascade.detectMultiScale(gray,1.2, 6)
         # Draw the rectangle around each face
-        for (x, y, w, h) in faces:
+        for (x, y, w, h) in eyes:
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
         # Display
         sendData(1)
+        mode = 1
     # Display
     cv2.imshow('Show Output - by Sukarna Jana', img)
     # Stop if escape key is pressed
+
+    if(mode==0):
+        moved+=1
+
+    if(moved>40):
+        sendData(11)
+        moved = 0
+
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
-    sendData(0)
+    mode = 0
 # Release the VideoCapture object
 cam.release()
