0ed01b60090843b44b097aad1f417e9fbd3ba84a
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2022-04-10 17:48:42 +0530

    Update carPart.ino

--- PU_P1/carPart.ino
+++ PU_P1/carPart.ino
@@ -5,39 +5,55 @@
 int j = 0;
 
 void setup(){
-  Serial.begin(9600);
 
   pinMode(startMove,OUTPUT);
   pinMode(leftTurn,OUTPUT);
   pinMode(errorLED,OUTPUT);
 
+  Serial.begin(9600);
+  Serial.println("© Sukarna Jana 2022");
+  Serial.println("1. In D13 pin LED blinks then some error has happened...");
+  Serial.println("2. Command 11 for error");
+  Serial.println("3. Command 10 for stop");
+  Serial.println("4. If Command 10 called 5 times then move the car to left");
+  Serial.println("---------------------------------------------------------");
+  
 }
 
 void loop(){
-  if (Serial.available() > 0) {
-    int info = Serial.read();
-    if(info==10){
+  if (Serial.available()) {
+    int infoFromBoard = Serial.parseInt();
+    Serial.print("Data is :");
+    Serial.println(infoFromBoard);
+    if(infoFromBoard==11){
+      digitalWrite(startMove,1);
+      Serial.println("Stoping");
+      j++;
+      delay(10);
+    }
+    else if(infoFromBoard==10){
+      Serial.println("Something went wrong");
       error();
     }
-    if(info==11){
-      digitalWrite(startMove,0);
-      j++;
-      delay(100);
-    }
     else{
-      digitalWrite(startMove,1);
+      digitalWrite(startMove,0);
+      Serial.println("Moving");
     }
     if(j>5){
-      digitalWrite(startMove,1);
-      digitalWrite(leftTurn,1);
+      digitalWrite(startMove,0);
+      Serial.print("Moving");
+      digitalWrite(leftTurn,0);
+      Serial.println("and moving to left");
+      delay(100);
       j = 0;
     }
   }
   else{
+    digitalWrite(startMove,1);
     error();
   }
-  digitalWrite(leftTurn,0);
-  delay(100);
+  digitalWrite(leftTurn,1);
+  delay(50);
 }
 
 void error(){
