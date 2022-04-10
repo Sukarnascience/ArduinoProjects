000b9ab89c8a4c963de83432d965211942dbee37
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2022-04-10 19:20:20 +0530

    Create carPart.ino

--- PU_P1/carPart.ino
+++ PU_P1/carPart.ino
@@ -1,6 +1,8 @@
-#define startMove 5
-#define leftTurn 4
+#define startMove 10
+#define leftTurn 11
 #define errorLED 13
+#define beep 12
+#define motion 9
 
 int j = 0;
 
@@ -9,6 +11,8 @@ void setup(){
   pinMode(startMove,OUTPUT);
   pinMode(leftTurn,OUTPUT);
   pinMode(errorLED,OUTPUT);
+  pinMode(motion,INPUT);
+  pinMode(beep,OUTPUT);
 
   Serial.begin(9600);
   Serial.println("© Sukarna Jana 2022");
@@ -22,16 +26,26 @@ void setup(){
 
 void loop(){
   if (Serial.available()) {
-    int infoFromBoard = Serial.parseInt();
+    //int infoFromBoard = Serial.parseInt();
+    char infoFromBoard = Serial.read();
+    int detectMove = digitalRead(motion);
+    if(detectMove){
+      Serial.println("Motion Detect");
+    }
+    else{
+      Serial.println("NO Motion Detect");
+    }
     Serial.print("Data is :");
     Serial.println(infoFromBoard);
-    if(infoFromBoard==11){
+    //if(infoFromBoard==11){
+    if(infoFromBoard=='S' || detectMove==0){
       digitalWrite(startMove,1);
       Serial.println("Stoping");
       j++;
       delay(10);
     }
-    else if(infoFromBoard==10){
+    //else if(infoFromBoard==10){
+    else if(infoFromBoard=='E'){
       Serial.println("Something went wrong");
       error();
     }
@@ -44,6 +58,7 @@ void loop(){
       Serial.print("Moving");
       digitalWrite(leftTurn,0);
       Serial.println("and moving to left");
+      digitalWrite(beep,1);
       delay(100);
       j = 0;
     }
@@ -53,6 +68,7 @@ void loop(){
     error();
   }
   digitalWrite(leftTurn,1);
+  digitalWrite(beep,0);
   delay(50);
 }
 
