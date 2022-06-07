72075170dfdbf0935b0462e2e4b914933789e45a
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2022-06-07 13:05:22 +0530

    Update IPC011.ino

--- IPC011/IPC011.ino
+++ IPC011/IPC011.ino
@@ -1,6 +1,7 @@
 const int trigPin = 2;
 const int echoPin = 3;
 const int buzz = 7;
+const int vibrate = 4;
 
 // defines variables
 long duration;
@@ -9,6 +10,7 @@ int distance;
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(vibrate,OUTPUT);
 pinMode(buzz,OUTPUT);
 Serial.begin(9600); // Starts the serial communication
 }
@@ -32,13 +34,15 @@ distance= duration*0.034/2;
 // Prints the distance on the Serial Monitor
 Serial.print("Distance: ");
 Serial.println(distance);
-if(distance<=100){
+if(distance<=30){
   Serial.print(distance);
   digitalWrite(buzz,1);
+  digitalWrite(vibrate,1);
   tone(buzz,220,100);
 }
 else{
   digitalWrite(buzz,0);
+  digitalWrite(vibrate,0);
 }
 
 }
